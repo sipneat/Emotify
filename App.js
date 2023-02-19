0c253e416d6ce9aee83b581236b79d3f87a024b1
Jake Esperson <jake.esperson@outlook.com>
2023-02-18 16:40:06 -0800

    changed tsx to js and deleted redundent index

---
+++ App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import MainPage from './main.js';
+import SecondPage from './AppTwo.js'
+import { Text, View, StyleSheet, Button, Image, TextInput } from 'react-native';
+import Card from "./Card";
+
+//import { View, Text } from 'react-native';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {pageProp: 'main'};
+    //this.state = 
+  }
+
+  change(){
+    this.setState({pageProp: 'AppTwo'})
+  }
+
+  render() {
+    if(this.state.pageProp === 'main'){
+      return(
+        <MainPage onSubmit = { this.change.bind(this)}/>
+      );
+    }else{
+      return (
+        <SecondPage onSubmit = {this.change.bind(this)}/>
+        );
+    }
+  }
+}
